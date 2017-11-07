39533ca0d10e2043cfd9a2e1e85b85934b3b4cc5
Horacio Lisdero <lisderoh@gmail.com>
2017-11-07 17:09:18 -0300

    Implemented crearJugador()

--- src/fiuba/algo3/tp2/modelo/AlgoPoly.java
+++ src/fiuba/algo3/tp2/modelo/AlgoPoly.java
@@ -3,6 +3,10 @@ package fiuba.algo3.tp2.modelo;
 public class AlgoPoly {
 	Tablero tablero = new Tablero();
 	
+	public Jugador crearJugador() {
+		return new Jugador(tablero.getCasillaInicial());
+	}
+	
 	public void mover(Jugador jugador, int posiciones) {
 		Casilla siguiente = tablero.getCasillaSiguiente(jugador.getCasilla(), posiciones);
 		jugador.setCasilla(siguiente);

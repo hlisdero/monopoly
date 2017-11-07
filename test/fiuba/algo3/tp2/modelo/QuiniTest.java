cdba1db5cab9adc70544ffa28c6dc01bf7aa7cd9
Horacio Lisdero <lisderoh@gmail.com>
2017-11-07 14:59:15 -0300

    Changed package name. Added Quini class Added QuiniTest class

---
+++ test/fiuba/algo3/tp2/modelo/QuiniTest.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.tp2.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuiniTest {
+
+	@Test
+	public void newQuiniNotNull() {
+		assertNotNull(new Quini());
+	}
+
+}

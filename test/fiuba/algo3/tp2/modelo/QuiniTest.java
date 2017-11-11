d0a11d3fcb5051073f6acba7fabc422fc2192b80
Horacio Lisdero <lisderoh@gmail.com>
2017-11-11 11:36:09 -0300

    Corregidas pruebas de Quini y se agregaron más pruebas para Quini.

--- test/fiuba/algo3/tp2/modelo/QuiniTest.java
+++ test/fiuba/algo3/tp2/modelo/QuiniTest.java
@@ -13,32 +13,68 @@ public class QuiniTest {
 	}
 	
 	@Test
-	public void aplicarEfecto()
-	{
+	public void primeraVezAgregaCincuentaMilAlCapital() {
 		Quini quini = new Quini();
 		Jugador jugador = new Jugador(quini);
 		
 		quini.aplicarEfecto(jugador);
-		assertEquals(jugador.getCapital(), 150000, DELTA);
-		
-		quini.aplicarEfecto(jugador);
-		assertEquals(jugador.getCapital(), 180000, DELTA);
-		
-		quini.aplicarEfecto(jugador);
-		assertEquals(jugador.getCapital(), 180000, DELTA);
+		assertEquals(150000, jugador.getCapital(), DELTA);
 	}
 	
 	@Test
-	public void aplicarEfectoSobreJugadores()
-	{
+	public void segundaVezAgregaTreintaMilAlCapital() {
+		Quini quini = new Quini();
+		Jugador jugador = new Jugador(quini);
+		
+		quini.aplicarEfecto(jugador);
+		quini.aplicarEfecto(jugador);
+		assertEquals(180000, jugador.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void terceraVezNoAgregaNadaAlCapital() {
+		Quini quini = new Quini();
+		Jugador jugador = new Jugador(quini);
+		
+		quini.aplicarEfecto(jugador);
+		quini.aplicarEfecto(jugador);
+		quini.aplicarEfecto(jugador);
+		assertEquals(180000, jugador.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void segundoJugadorPrimeraVezCaeAgregaCincuentaMilAlCapital() {
+		Quini quini = new Quini();
+		Jugador jugador1 = new Jugador(quini);
+		Jugador jugador2 = new Jugador(quini);
+		
+		quini.aplicarEfecto(jugador1);		
+		quini.aplicarEfecto(jugador2);
+		assertEquals(150000, jugador2.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void segundoJugadorSegundaVezCaeAgregaTreintaMilAlCapital() {
 		Quini quini = new Quini();
 		Jugador jugador1 = new Jugador(quini);
 		Jugador jugador2 = new Jugador(quini);
 		
 		quini.aplicarEfecto(jugador1);
-		assertEquals(jugador1.getCapital(), 150000, DELTA);
-		
 		quini.aplicarEfecto(jugador2);
-		assertEquals(jugador2.getCapital(), 150000, DELTA);
+		quini.aplicarEfecto(jugador2);
+		assertEquals(180000, jugador2.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void segundoJugadorTerceraVezCaeNoAgregaNadaAlCapital() {
+		Quini quini = new Quini();
+		Jugador jugador1 = new Jugador(quini);
+		Jugador jugador2 = new Jugador(quini);
+		
+		quini.aplicarEfecto(jugador1);
+		quini.aplicarEfecto(jugador2);
+		quini.aplicarEfecto(jugador2);
+		quini.aplicarEfecto(jugador2);
+		assertEquals(180000, jugador2.getCapital(), DELTA);
 	}
 }

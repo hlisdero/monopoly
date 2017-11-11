8b7ec3fd64fa9b0849624aecda6ab5a78de9b470
Horacio Lisdero <lisderoh@gmail.com>
2017-11-11 10:11:27 -0300

    Reimplementado Quini aislando parte de la funcionalidad en PremioQuini

---
+++ src/fiuba/algo3/tp2/modelo/PremioQuini.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.tp2.modelo;
+
+class PremioQuini {
+	private static final int MONTO_PRIMERA_VEZ = 50000;
+	private static final int MONTO_SEGUNDA_VEZ = 30000;
+
+	void darPremio(Jugador jugador, int cantidadVeces) {
+		if (cantidadVeces == 1) {
+			jugador.agregarDinero(MONTO_PRIMERA_VEZ);
+		} else if (cantidadVeces == 2)	{
+			jugador.agregarDinero(MONTO_SEGUNDA_VEZ);
+		}
+	}
+}
--- src/fiuba/algo3/tp2/modelo/Quini.java
+++ src/fiuba/algo3/tp2/modelo/Quini.java
@@ -3,28 +3,18 @@ package fiuba.algo3.tp2.modelo;
 import java.util.HashMap;
 
 class Quini implements Casilla {
-
-	private HashMap<Jugador, Integer> map = new HashMap<Jugador, Integer>();
-	private int numeroDeTurno = 1;
-	private int montoDePrimeraVez = 50000;
-	private int montoDeSegundaVez = 30000;
-
+	private HashMap<Jugador, Integer> vecesQueCayeron = new HashMap<Jugador, Integer>();
+	private PremioQuini premio = new PremioQuini();
 	
 	@Override
 	public void aplicarEfecto(Jugador jugador) {
-		// TODO Auto-generated method stub
-		
-		if(map.get(jugador) == null)
-		{
-			jugador.agregarDinero(montoDePrimeraVez);
-			map.put(jugador, this.numeroDeTurno + 1);
+		if (!vecesQueCayeron.containsKey(jugador)) {
+			vecesQueCayeron.put(jugador, 1);
+		} else {
+			int cantidadVeces = vecesQueCayeron.get(jugador);
+			vecesQueCayeron.put(jugador, cantidadVeces + 1);
 		}
-		else if(map.get(jugador).intValue() == 2)
-		{
-			jugador.agregarDinero(montoDeSegundaVez);
-			map.put(jugador, map.get(jugador).intValue() + 1);
-		}
-		
+		premio.darPremio(jugador, vecesQueCayeron.get(jugador));
 	}
 	
 }

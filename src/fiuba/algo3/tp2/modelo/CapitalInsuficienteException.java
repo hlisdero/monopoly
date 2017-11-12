141fab0587add3b310af508dbde0c352cdce29a0
bmangia <bmangia@192.168.1.33>
2017-11-12 16:16:24 -0300

    se modif excepcion a RunTime excepction para obviar Try/catch

--- src/fiuba/algo3/tp2/modelo/CapitalInsuficienteException.java
+++ src/fiuba/algo3/tp2/modelo/CapitalInsuficienteException.java
@@ -1,5 +1,5 @@
 package fiuba.algo3.tp2.modelo;
 
-public class CapitalInsuficienteException extends Exception {
+public class CapitalInsuficienteException extends RuntimeException {
 	static final long serialVersionUID = 1L;
 }

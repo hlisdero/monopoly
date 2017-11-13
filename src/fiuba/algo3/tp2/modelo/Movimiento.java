e4c8b2d689b8bf85c1aa72d2104e9a8205d8686f
Horacio Lisdero <lisderoh@gmail.com>
2017-11-13 20:54:59 -0300

    Eliminados Movimiento, MovimientoCarcel, MovimientoNormal

--- src/fiuba/algo3/tp2/modelo/Movimiento.java
+++
@@ -1,8 +0,0 @@
-package fiuba.algo3.tp2.modelo;
-
-public interface Movimiento {
-
-	//La idea es operar dentro de tablero y evitar el aplicarEfecto() si el movimiento fue restringido.
-public int getMultiplicadorDeMovimiento();
-
-}

b6626972a9d2bdbe4b291501ba70b875c50dd369
Matias <Matias@Mati>
2017-11-12 23:07:14 -0300

    Cree la interface Movimiento y los objetos MovimientoCarcel y MovimientoNormarl que la implementan. Preliminares. La idea es operar dentro de tablero y evitar el aplicarEfecto() si el movimiento fue restringido.

---
+++ src/fiuba/algo3/tp2/modelo/Movimiento.java
@@ -0,0 +1,8 @@
+package fiuba.algo3.tp2.modelo;
+
+public interface Movimiento {
+
+	//La idea es operar dentro de tablero y evitar el aplicarEfecto() si el movimiento fue restringido.
+public int getMultiplicadorDeMovimiento();
+
+}
---
+++ src/fiuba/algo3/tp2/modelo/MovimientoNormal.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.tp2.modelo;
+
+public class MovimientoNormal implements Movimiento {
+
+	@Override
+	public int getMultiplicadorDeMovimiento() {
+		return 1;
+	}
+
+}

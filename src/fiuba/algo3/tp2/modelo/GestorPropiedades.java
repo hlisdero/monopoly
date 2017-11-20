ef62522609caccfc08e7bdd5644f1a1ec823ece5
Haouz <haouzi.jeremy@gmail.com>
2017-11-20 13:13:40 -0300

    Add metodo construirCasa y Hotel + get en GestorPropiedad

--- src/fiuba/algo3/tp2/modelo/GestorPropiedades.java
+++ src/fiuba/algo3/tp2/modelo/GestorPropiedades.java
@@ -1,9 +1,27 @@
 package fiuba.algo3.tp2.modelo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GestorPropiedades {
 	private ArrayList<Propiedad> propiedades = new ArrayList<Propiedad>();
+	private HashMap<Propiedad, Integer> cantidadDeCasasPorPropiedad = new HashMap<Propiedad, Integer>();
+	private HashMap<Propiedad, Integer> cantidadDeHotelesPorPropiedad = new HashMap<Propiedad, Integer>();
+	
+	public void construirCasa(Propiedad prop) {
+		if (!cantidadDeCasasPorPropiedad.containsKey(prop)) {
+			cantidadDeCasasPorPropiedad.put(prop, 1);
+		} else {
+			int cantidadDeCasas = cantidadDeCasasPorPropiedad.get(prop);
+			cantidadDeCasasPorPropiedad.put(prop, cantidadDeCasas + 1);
+		}
+	}
+	
+	public void construirHotel(Propiedad prop) {
+		if (!cantidadDeHotelesPorPropiedad.containsKey(prop)) {
+			cantidadDeHotelesPorPropiedad.put(prop, 1);
+		}
+	}
 	
 	public void agregarPropiedad(Propiedad prop) {
 		propiedades.add(prop);
@@ -23,5 +41,13 @@ public class GestorPropiedades {
 	
 	public int getCantidadPropiedades() {
 		return propiedades.size();
+	}
+	
+	public int getCantidadCasas(Propiedad prop) {
+		return cantidadDeCasasPorPropiedad.get(prop);
+	}
+	
+	public int getCantidadHotel(Propiedad prop) {
+		return cantidadDeHotelesPorPropiedad.get(prop);
 	}
 }

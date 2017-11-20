5bb30a018118974f1fce3d902be82fd688468837
Haouz <haouzi.jeremy@gmail.com>
2017-11-20 11:59:38 -0300

    Actualizasion del metodo aplicarEfecto de Propiedad sin construciones

--- src/fiuba/algo3/tp2/modelo/Propiedad.java
+++ src/fiuba/algo3/tp2/modelo/Propiedad.java
@@ -33,5 +33,8 @@ public class Propiedad extends Casilla {
 			jugador.getGestorPropiedades().agregarPropiedad(this);
 			jugador.restarDinero(precio);
 		}
+		else if (!noTienePropietario() && jugador.getCapital() >= this.precioDeAlquiler) {
+			jugador.restarDinero(precioDeAlquiler);
+		}
 	}
 }

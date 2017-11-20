e9c2363ecad1bbe3e2c3f17a58833f2046b7ac2f
Haouz <haouzi.jeremy@gmail.com>
2017-11-20 11:27:03 -0300

    Creacion de la clase test para Empresa

---
+++ test/fiuba/algo3/tp2/modelo/EmpresaTest.java
@@ -0,0 +1,18 @@
+package fiuba.algo3.tp2.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmpresaTest {
+
+	@Test
+	public void nuevaEmpresaNoNull() {
+		assertNotNull(new Empresa(0));
+	}
+	
+	
+	
+	
+
+}

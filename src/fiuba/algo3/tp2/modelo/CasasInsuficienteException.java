a4bf92f06e42b3b32e2b573899fc143a43429071
Horacio Lisdero <lisderoh@gmail.com>
2017-11-22 22:14:58 -0300

    CasasInsuficienteException NO debe printear un mensaje

--- src/fiuba/algo3/tp2/modelo/CasasInsuficienteException.java
+++ src/fiuba/algo3/tp2/modelo/CasasInsuficienteException.java
@@ -1,14 +1,5 @@
 package fiuba.algo3.tp2.modelo;
 
 public class CasasInsuficienteException extends Exception {
-
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
-	public CasasInsuficienteException(){
-		System.out.println("Debe construir 2 casas antes de construir un hotel !");
-	}
 }

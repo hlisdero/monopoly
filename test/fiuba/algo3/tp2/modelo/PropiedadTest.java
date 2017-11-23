e0f03070e6c44096fc09cfd2dff2d6e9e1666648
bmangia <bmangia@fe80:0:0:0:ec49:95ff:fe08:fcc%8>
2017-11-23 09:43:36 -0300

    Creacion propiedadTest

---
+++ test/fiuba/algo3/tp2/modelo/PropiedadTest.java
@@ -0,0 +1,38 @@
+package fiuba.algo3.tp2.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+
+public class PropiedadTest {
+	
+	@Test
+	public void propiedadSeCreaYNoTieneDuenio() {
+		Propiedad propiedad = new Propiedad(1000);
+		assertTrue(propiedad.noTienePropietario());
+		
+	}
+	
+	@Test
+	public void propiedadTieneDuenio() {
+		Propiedad propiedad = new Propiedad(1000);
+		Jugador jugador = new Jugador(new Casilla());
+		propiedad.setPropietario(jugador);
+		assertTrue(propiedad.tienePropietario());
+	}
+	
+	@Test
+	public void propiedadCambiaDeDuenio() {
+		Propiedad propiedad = new Propiedad(1000);
+		Jugador jugador1 = new Jugador(new Casilla());
+		propiedad.setPropietario(jugador1);
+		propiedad.jugadorVendePropiedad(jugador1);
+		assertTrue(propiedad.noTienePropietario());
+	}
+
+
+
+}

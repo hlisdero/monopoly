fd995e077642434336cd86592c4c528d06fdb17c
Horacio Lisdero <lisderoh@gmail.com>
2017-11-23 18:53:35 -0300

    Creado paquete controlador. Movido AlgoPoly

--- src/fiuba/algo3/tp2/controlador/AlgoPoly.java
+++ src/fiuba/algo3/tp2/controlador/AlgoPoly.java
@@ -1,4 +1,8 @@
-package fiuba.algo3.tp2.modelo;
+package fiuba.algo3.tp2.controlador;
+
+import fiuba.algo3.tp2.modelo.GestorMovimiento;
+import fiuba.algo3.tp2.modelo.GestorTurnos;
+import fiuba.algo3.tp2.modelo.Jugador;
 
 public class AlgoPoly {
 	private GestorMovimiento gestorMovimiento = new GestorMovimiento();

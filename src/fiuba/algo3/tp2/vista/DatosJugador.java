f9d3c737cb104cc8d48eb26e663ea1ad42b2d593
Jeremy Haouzi <haouzi.jeremy@gmail.com>
2017-11-27 14:29:36 -0300

    Type for tableColumn

--- src/fiuba/algo3/tp2/vista/DatosJugador.java
+++ src/fiuba/algo3/tp2/vista/DatosJugador.java
@@ -9,20 +9,22 @@ public class DatosJugador extends Parent{
 	private TableView<String> table = new TableView<String>();
 	
 	public DatosJugador(){
-		TableColumn colJugador1 = new TableColumn("Jugador 1");
+		TableColumn<String, Integer> capital = new TableColumn<String, Integer>("Capital");
+		
+		TableColumn<String, Integer> colJugador1 = new TableColumn<String, Integer>("Jugador 1");
 		//firstNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
 		colJugador1.setMinWidth(100);
-		TableColumn colJugador2 = new TableColumn("Jugador 2");
+		TableColumn<String, Integer> colJugador2 = new TableColumn<String, Integer>("Jugador 2");
 	//	lastNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
 		colJugador2.setMinWidth(100);
-		TableColumn colJugador3 = new TableColumn("Jugador 3");
+		TableColumn<String, Integer> colJugador3 = new TableColumn<String, Integer>("Jugador 3");
 	//	phoneCol.setCellValueFactory(new PropertyValueFactory<Person, String>("phone"));
 		colJugador3.setMinWidth(100);
 
 		//table.setItems(data);
-		table.getColumns().addAll(colJugador1, colJugador2, colJugador3);
-
+		capital.getColumns().addAll(colJugador1, colJugador2, colJugador3);
 		
+		table.getColumns().add(capital);
 		table.setLayoutX(180);
 		table.setLayoutY(400);
 		

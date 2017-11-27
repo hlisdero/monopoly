f4ebe760e40cd1b9d43fda1e9b85dca70dc4ccb5
Jeremy Haouzi <haouzi.jeremy@gmail.com>
2017-11-27 12:23:31 -0300

    Refact tableroControl + boton tirarDados handler

--- src/fiuba/algo3/tp2/vista/TableroControl.java
+++ src/fiuba/algo3/tp2/vista/TableroControl.java
@@ -4,17 +4,14 @@ package fiuba.algo3.tp2.vista;
 
 import fiuba.algo3.tp2.vista.eventos.BotonComprarHandler;
 import fiuba.algo3.tp2.vista.eventos.BotonFinalizarTurnoHandler;
+import fiuba.algo3.tp2.vista.eventos.BotonTirarDadosHandler;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.stage.StageStyle;
 
 
 public class TableroControl extends Parent{
-	
 	
 	TerrenoVista terreno = new TerrenoVista();
 	
@@ -22,20 +19,14 @@ public class TableroControl extends Parent{
 	Button finalizarTurno= new Button("Finalizar turno");
 	Button comprarPropiedad = new Button("Comprar propiedad");
 	
-	private int sumaDados;
-	
 	private TurnoJugador turnoJugador = new TurnoJugador();
-	
 	
 	private int sizeListaJugador = turnoJugador.getListaJugadores().size();
 	
 	private TableView<String> table = new TableView<String>();
 	
 	public TableroControl(){
-		
-		
-		
-		
+
 		table.setLayoutX(50);
 		table.setLayoutY(400);
 
@@ -56,60 +47,27 @@ public class TableroControl extends Parent{
 
 		tirarDados.setLayoutX(370);
 		tirarDados.setLayoutY(400);
-		this.getChildren().add(tirarDados);
-		
+
 		finalizarTurno.setLayoutX(370);
 		finalizarTurno.setLayoutY(450);
-		this.getChildren().add(finalizarTurno);
 
-		
 		comprarPropiedad.setLayoutX(350);
 		comprarPropiedad.setLayoutY(500);
-		this.getChildren().add(comprarPropiedad);
 		
-		
-		
-       	tirarDados.setOnMouseClicked(e ->{
-	      
-	          	int numeroDeCasilla = turnoJugador.getJugadorGenerico().getNumeroCasilla();
-	          	
-	          	sumaDados = terreno.getAlgo().getGestorTurnos().proximoJugador().tirarDados().getSuma();
-	          	System.out.println(sumaDados);
-	          	numeroDeCasilla += sumaDados;
-	          	Alert dialogoAlerta = new Alert(AlertType.INFORMATION);
-	          	dialogoAlerta.setTitle("Resultado dados");
-	          	dialogoAlerta.setContentText("DADOS:" + sumaDados);
-	          	dialogoAlerta.initStyle(StageStyle.UTILITY);
-	          	dialogoAlerta.showAndWait();
-	          	
-	          	if(numeroDeCasilla > terreno.getList().size()){numeroDeCasilla -= terreno.getList().size() + 1;}
-	          	
-	          	turnoJugador.getJugadorGenerico().setNumeroCasilla(numeroDeCasilla);
-
-	          	
-	          	terreno.getAlgo().getGestorMovimiento().mover(turnoJugador.getJugadorGenerico().getValorJugador());
-	          	
-
-	          	turnoJugador.getJugadorGenerico().setTranslateX(terreno.getList().get(numeroDeCasilla).getPosX() + turnoJugador.getJugadorGenerico().posReferencia());
-
-	          	turnoJugador.getJugadorGenerico().setTranslateY(terreno.getList().get(numeroDeCasilla).getPosY());
-	           	
-	          	terreno.getAlgo().getGestorMovimiento().mover(turnoJugador.getJugadorGenerico().getValorJugador(), terreno.getList().get(turnoJugador.getJugadorGenerico().getNumeroCasilla()).getValorCasilla());
-	          	
-	          	//tirarDados.setDisable(true);
-	            });
-       	
+		BotonTirarDadosHandler tirarDadosHandler = new BotonTirarDadosHandler(turnoJugador, terreno);
+       	tirarDados.setOnAction(tirarDadosHandler);
+	    //tirarDados.setDisable(true););
        	
        	BotonComprarHandler comprarHandler = new BotonComprarHandler(turnoJugador.getJugadorGenerico(), terreno);
     	comprarPropiedad.setOnAction(comprarHandler);
-    	//comprarPropiedad.setDisable(true);
-    		
+    	//comprarPropiedad.setDisable(true);	
 
        	BotonFinalizarTurnoHandler finalizarHandler = new BotonFinalizarTurnoHandler(turnoJugador, turnoJugador.getJugadorGenerico(), sizeListaJugador);
        	finalizarTurno.setOnAction(finalizarHandler);
 		//tirarDados.setDisable(false);
 		//comprarPropiedad.setDisable(false);
        	
+       	this.getChildren().addAll(tirarDados,finalizarTurno,comprarPropiedad);
        	this.getChildren().add(terreno);
 		this.getChildren().add(turnoJugador);
 	}
---
+++ src/fiuba/algo3/tp2/vista/eventos/BotonTirarDadosHandler.java
@@ -0,0 +1,53 @@
+package fiuba.algo3.tp2.vista.eventos;
+
+import fiuba.algo3.tp2.vista.TerrenoVista;
+import fiuba.algo3.tp2.vista.TurnoJugador;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.StageStyle;
+
+public class BotonTirarDadosHandler implements EventHandler<ActionEvent>{
+
+	private TurnoJugador turno;
+	private TerrenoVista terreno;
+	private int sumaDados;
+	private int numeroCasilla;
+	
+	public BotonTirarDadosHandler(TurnoJugador turno, TerrenoVista terreno){
+		
+		this.turno = turno;
+		this.terreno = terreno;
+	}
+	
+	
+	@Override
+    public void handle(ActionEvent actionEvent) {
+		numeroCasilla = turno.getJugadorGenerico().getNumeroCasilla();
+  
+		sumaDados = terreno.getAlgo().getGestorTurnos().proximoJugador().tirarDados().getSuma();
+      	System.out.println(sumaDados);
+      	numeroCasilla += sumaDados;
+      	Alert dialogoAlerta = new Alert(AlertType.INFORMATION);
+      	dialogoAlerta.setTitle("Resultado dados");
+      	dialogoAlerta.setContentText("DADOS:" + sumaDados);
+      	dialogoAlerta.initStyle(StageStyle.UTILITY);
+      	dialogoAlerta.showAndWait();
+      	
+      	if(numeroCasilla > terreno.getList().size()){numeroCasilla -= terreno.getList().size() + 1;}
+      	
+      	turno.getJugadorGenerico().setNumeroCasilla(numeroCasilla);
+
+      	
+      	terreno.getAlgo().getGestorMovimiento().mover(turno.getJugadorGenerico().getValorJugador());
+      	
+      	turno.getJugadorGenerico().setTranslateX(terreno.getList().get(numeroCasilla).getPosX() + turno.getJugadorGenerico().posReferencia());
+
+      	turno.getJugadorGenerico().setTranslateY(terreno.getList().get(numeroCasilla).getPosY());
+       	
+      	terreno.getAlgo().getGestorMovimiento().mover(turno.getJugadorGenerico().getValorJugador(), terreno.getList().get(turno.getJugadorGenerico().getNumeroCasilla()).getValorCasilla());
+      	
+    }
+	
+}

638c7b2d274f089e0e41dcd8ca5580e70f14bbf1
Horacio Lisdero <lisderoh@gmail.com>
2017-11-30 01:34:09 -0300

    Agregadas y modificadas pruebas para Provincia

--- test/fiuba/algo3/tp2/modelo/ProvinciaTest.java
+++ test/fiuba/algo3/tp2/modelo/ProvinciaTest.java
@@ -8,57 +8,129 @@ public class ProvinciaTest {
 	private static final double DELTA = 1e-15;
 	
 	@Test
-	public void nuevaProvinciaNoNull() {
-		assertNotNull(new Provincia(0, 0, 0, 0, null));
+	public void nuevaProvinciaSimpleNoNull() {
+		assertNotNull(new Provincia(0, 0, 0, 0, ""));
+	}
+	
+	@Test
+	public void nuevaProvinciaDobleNoNull() {
+		assertNotNull(new Provincia(0, 0, 0, 0, 0, 0, 0, ""));
+	}
+	
+	@Test
+	public void existeProvinciaHermanaInicialDevuelveFalse() {
+		Provincia provincia = new Provincia(0, 0, 0, 0, "");
+		assertFalse(provincia.existeProvinciaHermana());
+	}
+	
+	@Test
+	public void existeProvinciaHermanaInicialDevuelveTrueSiSetProvinciaHermana() {
+		Provincia provincia = new Provincia(0, 0, 0, 0, "");
+		Provincia provinciaHermana = new Provincia(0, 0, 0, 0, "");
+		
+		provincia.setProvinciaHermana(provinciaHermana);
+		assertTrue(provincia.existeProvinciaHermana());
+	}
+	
+	@Test
+	public void sePuedeConstruirHotelInicialDevuelveFalse() {
+		Provincia provincia = new Provincia(0, 0, 0, 0, "");
+		assertFalse(provincia.sePuedeConstruirHotel());
 	}
 	
 	@Test
 	public void getPrecioDevuelvePrecio() {
-		Provincia prop = new Provincia(1000, 0, 0, 0, null);
-		assertEquals(1000, prop.getPrecio(), DELTA);
+		Provincia provincia = new Provincia(1000, 0, 0, 0, "");
+		assertEquals(1000, provincia.getPrecio(), DELTA);
+	}
+	
+	@Test
+	public void aplicarEfectoNoCobraSiNoHayPropietario() {
+		Provincia provincia = new Provincia(1000, 500, 0, 0, "");
+		Jugador jugador = new Jugador(provincia);		
+		
+		provincia.aplicarEfecto(jugador);
+		assertEquals(100000, jugador.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void aplicarEfectoNoCobraSiCaePropietario() {
+		Provincia provincia = new Provincia(0, 500, 0, 0, "");
+		Jugador jugador = new Jugador(provincia);
+		
+		provincia.comprar(jugador);
+		provincia.aplicarEfecto(jugador);
+		assertEquals(100000, jugador.getCapital(), DELTA);
 	}
 		
 	@Test
 	public void aplicarEfectoCobraSiYaTieneUnPropietario() {
-		Provincia prop = new Provincia(1000, 500, 2000, 1000, null);
-		Jugador jugador1 = new Jugador(prop);
-		Jugador jugador2 = new Jugador(prop);
+		Provincia provincia = new Provincia(1000, 500, 2000, 1000, "");
+		Jugador jugador1 = new Jugador(provincia);
+		Jugador jugador2 = new Jugador(provincia);
 		
-		prop.setPropietario(jugador1);
-		prop.aplicarEfecto(jugador2);
+		provincia.comprar(jugador1);
+		provincia.aplicarEfecto(jugador2);
 		assertEquals(99500, jugador2.getCapital(), DELTA);
 	}
 	
 	@Test
-	public void construirCasaCapitalJugadorSeReduce() throws ConstruirCasaInvalidoException {
-		Provincia prop = new Provincia(20000, 2000, 5000, 8000, 3000, 3500, 5000, null);
-		Jugador jugador1 = new Jugador(prop);
+	public void aplicarEfectoCobraPrecioAlquilerCasa() throws ConstruirCasaInvalidoException {
+		Provincia provincia = new Provincia(1000, 500, 2000, 1000, "");
+		Jugador jugador1 = new Jugador(provincia);
+		Jugador jugador2 = new Jugador(provincia);
 		
-		prop.setPropietario(jugador1);
-		prop.construirCasa();	
-		assertEquals(95000, jugador1.getCapital(), DELTA);
+		provincia.comprar(jugador1);
+		provincia.construirCasa();
+		provincia.aplicarEfecto(jugador2);
+		assertEquals(99000, jugador2.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void aplicarEfectoCobraPrecioAlquilerHotel() throws ConstruirCasaInvalidoException, ConstruirHotelInvalidoException {
+		Provincia provincia = new Provincia(0, 0, 0, 0, 0, 0, 10000, "");
+		Jugador jugador1 = new Jugador(provincia);
+		Jugador jugador2 = new Jugador(provincia);
+		
+		provincia.comprar(jugador1);
+		provincia.construirCasa();
+		provincia.construirCasa();
+		provincia.construirHotel();
+		provincia.aplicarEfecto(jugador2);
+		assertEquals(90000, jugador2.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void construirCasaCapitalJugadorSeReduce() throws ConstruirCasaInvalidoException {
+		Provincia provincia = new Provincia(0, 0, 2000, 0, 0, 0, 0, "");
+		Jugador jugador1 = new Jugador(provincia);
+		
+		provincia.comprar(jugador1);
+		provincia.construirCasa();	
+		assertEquals(98000, jugador1.getCapital(), DELTA);
+	}
+	
+	@Test
+	public void construirCasaSegundaVezCapitalJugadorSeReduce() throws ConstruirCasaInvalidoException {
+		Provincia provincia = new Provincia(0, 0, 2000, 0, 0, 0, 0, "");
+		Jugador jugador = new Jugador(provincia);
+		
+		provincia.comprar(jugador);
+		provincia.construirCasa();
+		provincia.construirCasa();
+		assertEquals(96000, jugador.getCapital(), DELTA);
 	}
 	
 	@Test
 	public void construirHotelCapitalJugadorSeReduce() throws ConstruirCasaInvalidoException, ConstruirHotelInvalidoException {
-		Provincia prop = new Provincia(20000, 2000, 5000, 8000, 3000, 3500, 5000, null);
-		Jugador jugador1 = new Jugador(prop);
+		Provincia provincia = new Provincia(0, 0, 0, 8000, 0, 0, 0, "");
+		Jugador jugador = new Jugador(provincia);
 		
-		prop.setPropietario(jugador1);
-		prop.construirCasa();
-		prop.construirCasa();
-		prop.construirHotel();
-		assertEquals(82000, jugador1.getCapital(), DELTA);
+		provincia.comprar(jugador);
+		provincia.construirCasa();
+		provincia.construirCasa();
+		provincia.construirHotel();
+		assertEquals(92000, jugador.getCapital(), DELTA);
 	}
 	
-	@Test
-	public void venderPropiedadSeteaPropietarioNull() {
-		Provincia prop = new Provincia(1000,500,0,0, null);
-		Jugador jugador1 = new Jugador(prop);
-		
-		prop.aplicarEfecto(jugador1);
-		prop.comprar(jugador1);
-		prop.vender(jugador1);
-		assertNull(prop.getPropietario());
-	}	
 }

2e2618e5974d8e2221b117da2ecd1714490b372b
Horacio Lisdero <lisderoh@gmail.com>
2017-11-30 21:43:35 -0300

    Agregados métodos a AlgoPoly

--- src/fiuba/algo3/tp2/controlador/AlgoPoly.java
+++ src/fiuba/algo3/tp2/controlador/AlgoPoly.java
@@ -6,6 +6,7 @@ import fiuba.algo3.tp2.modelo.Casilla;
 import fiuba.algo3.tp2.modelo.GestorMovimiento;
 import fiuba.algo3.tp2.modelo.GestorTurnos;
 import fiuba.algo3.tp2.modelo.Jugador;
+import fiuba.algo3.tp2.modelo.Propiedad;
 
 public class AlgoPoly {
 	private GestorMovimiento gestorMovimiento = new GestorMovimiento();
@@ -16,8 +17,8 @@ public class AlgoPoly {
 		gestorMovimiento.mover(jugador);
 	}
 
-	public void mover(int espacios,Jugador jugador) {
-		gestorMovimiento.mover(jugador,espacios);
+	public void mover(Jugador jugador, int posiciones) {
+		gestorMovimiento.mover(jugador, posiciones);
 	}
 	
 	public Jugador proximoJugador() {
@@ -25,10 +26,21 @@ public class AlgoPoly {
 		gestorMovimiento.notificarNuevoTurno(jugadorActual);
 		return jugadorActual;
 	}
+	
+	public boolean sePuedeComprar(Propiedad propiedad) {
+		return propiedad.sePuedeComprar();
+	}
+	
+	public void comprar(Jugador jugador, Propiedad propiedad) {
+		jugador.comprar(propiedad);
+	}
+	
+	public void vender(Jugador jugador, Propiedad propiedad) {
+		jugador.vender(propiedad);
+	}
 
 	public Jugador getJugador() {
-		jugadorActual  = gestorTurnos.proximoJugador();
-		return jugadorActual;
+		return gestorTurnos.proximoJugador();
 	}
 
 	public ArrayList<Casilla> getCasillas() {
@@ -36,9 +48,6 @@ public class AlgoPoly {
 	}
 
 	public void notificarNuevoTurno(Jugador jugador) {
-		gestorMovimiento.notificarNuevoTurno(jugador);
-		
+		gestorMovimiento.notificarNuevoTurno(jugador);	
 	}
-	
-	
 }

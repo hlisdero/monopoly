b8f25946e279fc3e890ed0bfb2bf4fe238fc372d
Matias Levi <matilevi35@gmail.com>
2017-11-30 21:05:44 -0300

    para tests de javaFx Threading Rule -> RunWith

--- test/fiuba/algo3/tp2/vista/TerrenoVistaTest.java
+++ test/fiuba/algo3/tp2/vista/TerrenoVistaTest.java
@@ -2,15 +2,14 @@ package fiuba.algo3.tp2.vista;
 
 import static org.junit.Assert.*;
 
-import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import fiuba.algo3.tp2.controlador.AlgoPoly;
 
-
+@RunWith( JfxTestRunner.class )
 public class TerrenoVistaTest {
-		@Rule 
-		public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+
 		@Test
 		public void nuevoTerrenoVistaNoEsNull() {
 			TerrenoVista terreno = new TerrenoVista();
